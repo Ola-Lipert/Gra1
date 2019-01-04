506b4098b4e1f190cb28d7c913191e40387c340d
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-05 00:08:47 +0100

    Add order in the code

--- js/index.js
+++ js/index.js
@@ -5,8 +5,6 @@ var button2 = document.getElementById('button_2');
 var button3 = document.getElementById('button_3');
 var newGame = document.getElementById('newGame');
 
-
-
 var params = {
   playersWin: 0,
   computersWin: 0,
@@ -23,57 +21,29 @@ var params = {
 //dodanie tekstu
 params.output.innerHTML = 'START GAME!! Click the button!' + '<br><br>' + params.output.innerHTML;
 
-
 // Pętla przechodzącą przez wszystkie elementy z klasą player-move
 
   var allPlayerMove = document.querySelectorAll('.player-move'); 
 
   for (var i = 0; i < allPlayerMove.length; i++) {
-    
-  
     allPlayerMove[i].addEventListener('click', function() { 
       var dataMove = this.getAttribute('data-move'); 
       playerMove(dataMove);
     });
   }
 
-
-/*
-var allPlayerMove = document.querySelectorAll('.player-move'); // Ustalam zmienną dla wszystkich elementów, które mają klasę "player-move"
-
-for (var i = 0; i < allPlayerMove.length; i++) {
-  var dataMove = allPlayerMove[i].getAttribute('data-move'); // Tworzę pętlę, która wyciąga z przeszukanych elementów atrybut "data-move"
-  
-  allPlayerMove[i].addEventListener('click', function() { // Wywołuję proces nasłuchiwania i wykonuję funkcję z argumentem "dataMove"
-    
-    playerMove(dataMove);
-  });
-}
-button1.addEventListener('click', function() {
-  playerMove('paper');
-});
-
-button2.addEventListener('click', function() {
-  playerMove('stone');
-});
-
-button3.addEventListener('click', function() {
-  playerMove('scissors');
-});
-*/
-
 //Funkcja która losuje 
 function randomNumber() {
   var computerChoices = ['paper', 'stone', 'scissors'];
   var computerMove = Math.floor(Math.random() * 3); //gdy było +1 w przypadku wylosowania paper undefined
-  return computerChoices[computerMove];
+    return computerChoices[computerMove];
 };
 
 function playerMove(playerChoice, computerChoice) {
-
   var winner;
 
   computerChoice = randomNumber();
+
   if (playerChoice == computerChoice) {
     params.output.innerHTML = 'It is tie!';
     winner = 'tie';
@@ -124,21 +94,17 @@ function gameOver() {
     showModal();
     document.querySelector('.result-modal').innerHTML = 'GAME OVER! Press the new game button to start';
     disableButton(true);
-    
   }
 
   var html = '<table>';
 
   for (var i = 0; i < params.progress.length; i++) {
-
     html += '<tr><td>' + (i+1) + '</td><td>' + params.progress[i].player + '</td><td>' + params.progress[i].computer + '</td><td>' + params.progress[i].winner + '</td></tr>';
-
   }
 
   html += '</table>';
 
   document.querySelector('.result-table').innerHTML = html;
-
 };
 
 function disableButton(state) {                              //zablokowanie przycisków
@@ -157,43 +123,22 @@ newGame.addEventListener('click', function() {
   params.result.innerHTML = '';      //czyszczenie wyników
   params.gameResult.innerHTML = '';
   params.output.innerHTML = '';
-  
 });
-/*
-function tableGame(){
-  var html = '<table>';
 
-  for (var i = 0; i < params.progress.length; i++) {
-
-    html += '<tr><td>' + (i+1) + '</td><td>' + params.progress[i].player + '</td><td>' + params.progress[i].computer + '</td><td>' + params.progress[i].winner + '</td></tr>';
-
-  }
-
-  html += '</table>';
-
-  document.querySelector('.result-modal').innerHTML = html;
-
-}
-*/
 //Modal
 function showModal(){
   var allModals = document.querySelectorAll('.modal');
-	  for (var i = 0; i < allModals.length; i++) {
-      allModals[i].classList.remove('show');
-    
-	  }
+
+	for (var i = 0; i < allModals.length; i++) {
+    allModals[i].classList.remove('show');
+  }
   document.querySelector('#modal-overlay').classList.add('show');
-    
- 
 };
 
-
-// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
-
+// Funkcję zamykająca modal, oraz przywiązanie jej do kliknięć na elemencie z klasą "close". 
 var hideModal = function(event){
   event.preventDefault();
   document.querySelector('#modal-overlay').classList.remove('show');
-  
 };
 
 var closeButtons = document.querySelectorAll('.modal .close');
@@ -202,11 +147,10 @@ for(var i = 0; i < closeButtons.length; i++){
   closeButtons[i].addEventListener('click', hideModal);
 }
 
-// Dobrą praktyką jest również umożliwianie zamykania modala poprzez kliknięcie w overlay. 
-
+// Zamykania modala poprzez kliknięcie w overlay. 
 document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 
-// Musimy jednak pamiętać, aby zablokować propagację kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
+// Zablokowanie propagacji kliknięć z samego modala - inaczej każde kliknięcie wewnątrz modala również zamykałoby go. 
 
 var modals = document.querySelectorAll('.modal');
 
